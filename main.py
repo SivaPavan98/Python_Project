e24e3337236298147bc3f05bd807c53bb2d849c7
Siva <mallisettisivapavan@gmail.com>
2025-07-19 14:12:48 +0530

    Add description, imgs and Links

--- main.py
+++ main.py
@@ -9,20 +9,29 @@ with col1:
 
 with col2:
     st.title("TO DO APP")
-    content =""" This is TO DO Application created by Siva.
-    """
+
+    content =""" This is TO DO Application created by Siva."""
     st.info(content)
 
-content2 = """
-Below you can find some apps I have built in python. Feel free to contact me...!"""
+content2 = """Below you can find some apps I have built in python. Feel free to contact me...!"""
 
 st.info(content2)
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([2,0.3,2])
 
 df = pandas.read_csv("data.csv", sep=";")
+
 with col3 :
-    for index, row in df[10:].iterrows():
-        st.header(row["title"])
-with col4 :
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.markdown(f"[{row['title']}]({row['url']})")
+
+
+with col4 :
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row ["image"])
+        st.markdown(f"[{row['title']}]({row['url']})")
+
